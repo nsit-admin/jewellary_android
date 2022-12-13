95621ecd5a5900fc9f3539dac56839f69c61a9e7
Baskar <it.baskar@gmail.com>
2022-12-12 22:21:05 -0800

    New skeleton project created
    New skeleton project created

--- ccavenue/README.md
+++
@@ -1,1 +0,0 @@
-# jewellary_android
--- mobile/screens/index.js
+++
@@ -1,5 +0,0 @@
-import AuthScreen from './AuthScreen';
-import MyChitsScreen from './MyChitsScreen';
-import AddSchemeScreen from './AddSchemeScreen';
-
-export { AuthScreen, MyChitsScreen, AddSchemeScreen };
