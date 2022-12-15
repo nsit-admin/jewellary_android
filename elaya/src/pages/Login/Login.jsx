47ea7e3a69ae357e6ec48b338fbc112eacd0f043
Elayaraja <raja5ramani@gmail.com>
2022-12-15 16:52:38 +0530

    Add files via upload

---
+++ elaya/src/pages/Login/Login.jsx
@@ -0,0 +1,33 @@
+import React from "react"
+import { Link } from "react-router-dom"
+import "./Login.css"
+
+const Login = () => {
+  return (
+    <div className="login">
+      <img src="img/logo.png" alt="logo" className="logo" />
+      <div className="container">
+        <img src="img/login-bg.jpeg" alt="login" />
+        <div className="form">
+          <h1>Login</h1>
+          <span>Please signin to your account</span>
+          <div className="formContainer">
+            <input type="text" name="username" id="username" />
+            <input type="password" name="password" id="password" />
+            <Link className="forgotPassword">
+              <span>Forgot password?</span>
+            </Link>
+            <Link to="/existing-scheme">
+              <button type="button">Signin</button>
+            </Link>
+            <span>
+              Don't have an account? <Link className="signup">Signup</Link>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Login
